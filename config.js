e6e5eac14d0388ed66ca378d6aa3d9905e72ab61
Liat Deutcsh <liat.chen0@gmail.com>
2021-11-22 02:06:18 +0200

    start-project

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "a77d071c0be64d58c400fb74f6a1541c",
+    },
+};
+
+module.exports = constants;
